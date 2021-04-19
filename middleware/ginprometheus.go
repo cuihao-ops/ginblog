ec7e90c18e164059de4f4ad53eb85e07b91ac54d
cuihao <cuihao@shiqiao.com>
2021-04-19 13:28:18 +0800

    添加注释

--- middleware/ginprometheus.go
+++ middleware/ginprometheus.go
@@ -25,8 +25,9 @@ var (
 		Name:        "histogram",
 		Help:        "Histogram of response latency (seconds) of http handlers.",
 		ConstLabels: nil,
+		//Buckets: []float64{},
 		//默认分组时间{0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10}
-		Buckets: []float64{0.2, 0.5, 1, 2, 5, 10, 30},
+		Buckets: []float64{0.2, 0.5, 1, 2, 5, 10, 30}, //自定义分组时间
 	}, []string{"method", "code", "uri", application})
 
 	httpCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
@@ -38,11 +39,13 @@ var (
 	}, []string{"method", "code", "uri", application})
 
 	httpSummary = prometheus.NewSummaryVec(prometheus.SummaryOpts{
-		Namespace:   "http_server",
-		Subsystem:   "",
-		Name:        "Summary",
-		Help:        "Summary of response latency (seconds) of http handlers.",
-		Objectives:  map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+		Namespace: "http_server",
+		Subsystem: "",
+		Name:      "Summary",
+		Help:      "Summary of response latency (seconds) of http handlers.",
+		// Objectives:  map[float64]float64{},
+		// 默认的话就是sum
+		Objectives:  map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001}, //定义分位数
 		ConstLabels: nil,
 	}, []string{"method", "code", "uri", application})
 )
