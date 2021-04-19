583cb1d5c96c06de660349ec88c2c4f04422e63d
cuihao <cuihao@shiqiao.com>
2021-04-19 13:21:21 +0800

    添加summary指标

--- middleware/ginprometheus.go
+++ middleware/ginprometheus.go
@@ -25,21 +25,31 @@ var (
 		Name:        "histogram",
 		Help:        "Histogram of response latency (seconds) of http handlers.",
 		ConstLabels: nil,
-		Buckets:     nil,
+		//默认分组时间{0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10}
+		Buckets: []float64{0.2, 0.5, 1, 2, 5, 10, 30},
 	}, []string{"method", "code", "uri", application})
 
 	httpCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace:   "http_server",
 		Subsystem:   "",
-		Name:        "counter",
-		Help:        "counter of response latency (seconds) of http handlers.",
+		Name:        "Counter",
+		Help:        "Counter of response latency (seconds) of http handlers.",
+		ConstLabels: nil,
+	}, []string{"method", "code", "uri", application})
+
+	httpSummary = prometheus.NewSummaryVec(prometheus.SummaryOpts{
+		Namespace:   "http_server",
+		Subsystem:   "",
+		Name:        "Summary",
+		Help:        "Summary of response latency (seconds) of http handlers.",
+		Objectives:  map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
 		ConstLabels: nil,
 	}, []string{"method", "code", "uri", application})
 )
 
 // init 初始化prometheus模型
 func init() {
-	prometheus.MustRegister(httpHistogram, httpCounter)
+	prometheus.MustRegister(httpHistogram, httpCounter, httpSummary)
 }
 
 // handlerPath 定义采样路由struct
@@ -136,8 +146,15 @@ func (gp *GinPrometheus) Middleware() gin.HandlerFunc {
 		httpCounter.WithLabelValues(
 			c.Request.Method,
 			strconv.Itoa(c.Writer.Status()),
-			gp.application,
 			gp.pathMap.get(c.HandlerName()),
+			gp.application,
 		).Inc()
+
+		httpSummary.WithLabelValues(
+			c.Request.Method,
+			strconv.Itoa(c.Writer.Status()),
+			gp.pathMap.get(c.HandlerName()),
+			gp.application,
+		).Observe(time.Since(start).Seconds())
 	}
 }
